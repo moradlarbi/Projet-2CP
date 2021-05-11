5bdc345e8582f7f72843f6a603624f44173280ad
moradlarbi <moradlarbi1@gmail.com>
2021-05-11 16:20:21 +0100

    users.txt

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,14 +4,13 @@ const mysql = require('mysql')
 const bcrypt = require('bcryptjs')
 const router = express.Router()
 
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '123456789',
-//     database: 'projet_bdd'
-// })
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456789',
+    database: 'projet_bdd'
+})
 var erreur = {}
-var i=2;
 router.post('/signup/', (req, res) => {
     var nm = req.body.nom
     var prnm = req.body.prenom
@@ -27,25 +26,19 @@ router.post('/signup/', (req, res) => {
     erreur.type = ''
     erreur.password = ''
     erreur.password2 = ''
-    console.log(adrss_email + ' ' + req.body.e_mail)
     //console.log(req.body)
     // VERIFIE LES CHAMPS 
-    console.log("--------------------" )
-    if (!nm){
+    if (!nm.trim()){
         erreur.username = 'Nom est requit'
         console.log('un champs est vide ')
     }
-    if(!prnm){
+    if(!prnm.trim()){
         erreur.userprenom = 'Prenom est requit'
-        console.log('prenom est vide ')
-    }
-    if(!adrss_email){
-        erreur.email = 'email est requit'
-        console.log('email est vide ')
+        console.log('prenom est requit ')
     }
     if(!srv){
         erreur.role = 'Role est requit'
-        console.log('role est vide ')
+        console.log('srv est vide ')
     }
     if(!type){
         erreur.type = 'Type est requit'
@@ -53,24 +46,26 @@ router.post('/signup/', (req, res) => {
     }
     if(!mp){
         erreur.password = 'Mot de passe est requit'
-        console.log('mdp est vide ')
+        console.log('modpsse est vide ')
     }
     if(!mp2){
         erreur.password2 = 'Mot de passe de confirmation est requit'
-        console.log('mdp2 est vide ')
+        console.log('confim est vide ')
     }
-    // VERIFIE L'ADRESSE EMAIL
-    if (!/\S+@\S+\.\S+/.test(adrss_email)){
-        erreur.email = 'Email invalid'
-        console.log('EMAIL INVALID')
-    }
-    // VERIFIER L'ADRESSE EMAIL ET LE MOT DE PASSE 
-    /*else {
+    // VERIFIER L'ADRESSE EMAIL ET LE MOT DE PASSE
+    if(!adrss_email){
+        erreur.email = 'email est requit'
+        console.log('email est vide ')
+    }else{
+        if (!/\S+@\S+\.\S+/.test(adrss_email)){
+            erreur.email = 'Email invalid'
+            console.log('EMAIL INVALID')
+        } 
+        else {
         db.query('SELECT email FROM user WHERE email = ?', [adrss_email], async (error, results) => {
             if (error) {console.log('la 1: '+error.message)}
 
             if (results.length > 0) {
-
                 erreur.email = 'EXISTE DEJA ';
                 console.log("existe deja")
                 ////
@@ -78,10 +73,10 @@ router.post('/signup/', (req, res) => {
             }      
             if (mp.length < 8){
                 erreur.password = 'MOT DE PASS Au moins 8 CARATERER'
-                 console.log('MOT DE PASS 8 CARATERER')
+                console.log('MOT DE PASS 8 CARATERER')
             }
             if (mp != mp2){
-                erreur.password2 = 'MOT DE PASS Incorrect'
+                erreur.password2 = 'MOT DE PASSE Incorrect'
                 console.log('MOT DE PASSE INCORRECT ')
             }
             if (mp != mp2 || results.length > 0 || mp.length <8){
@@ -99,43 +94,43 @@ router.post('/signup/', (req, res) => {
                 })
             }
         })
-    }*/
-    console.log(erreur)
+     }
+    }
 })
 var errors = {}
 var info = {}
-const httpProxy = require('http-proxy');
-const proxy = httpProxy.createServer({});
 router.post('/login/', async (req, res) => {
-    const adrss_email = req.body.e_mail 
-    const mp = req.body.mdpss
+    var adrss_email = req.body.e_mail 
+    var mp = req.body.mdpss
 
     console.log(req.body)
     // VERIFIE LES CHAMPS 
     errors.email = ''
     errors.password = ''
 
-    info.id = '1'
-    info.nom = 'morad'
-    info.prenom = 'larbi'
-    info.email = 'marche@esi.dz'
-    info.psswrd = '123456789'
-    info.service = 'marche'
-    info.role = 'consult'
+    info.id = ''
+    info.nom = ''
+    info.prenom = ''
+    info.email = ''
+    info.pssword = ''
+    info.service = ''
+    info.role = ''
     info.CT = ''
 
     if (!adrss_email ){
         errors.email = 'email est vide';
-        return console.log(errors.email)
+        console.log(errors.email)
     }
     if (!mp){
         errors.password = "mot de passe vide";
-        return console.log(errors.password)
-        
+        console.log(errors.password)   
     }
-    console.log(info.service);
-    
-    /*db.query('SELECT * FROM user WHERE email = ?', [adrss_email], async (error, results) => {
+    if (!adrss_email || !mp )
+    {
+        console.log('LE VIDE DANS LES 2')
+    }
+    else{
+        db.query('SELECT * FROM user WHERE email = ?', [adrss_email], async (error, results) => {
         if (error) {
             console.log(error.message)
         }
@@ -148,13 +143,15 @@ router.post('/login/', async (req, res) => {
             info.nom = results[0].nom
             info.prenom = results[0].prenom
             info.email = results[0].email
-            info.psswrd = results[0].password
+            info.pssword = results[0].password
             info.service = results[0].service
             info.role = results[0].type
             info.CT = results[0].create_time
             
             console.log('WELCOME TO YOUR PROFILE')
         }
-    })*/
+    })        
+    }
+
 })
 module.exports = {router, errors, erreur, info}
